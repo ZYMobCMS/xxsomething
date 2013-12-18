b845cb82588ddd6e69f9a933ccbebf27c39f8e50
zyprosoft <hutao5262705@hotmail.com>
2013-12-18 21:39:49 +0800

    add item

---
+++ XiaoXiao/XiaoXiao/AppMain/MainUI/CommonView/XXRadioChooseItem.h
@@ -0,0 +1,13 @@
+//
+//  XXRadioChooseItem.h
+//  XiaoXiao
+//
+//  Created by ZYVincent on 13-12-18.
+//  Copyright (c) 2013年 ZYProSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXRadioChooseItem : UIControl
+
+@end
